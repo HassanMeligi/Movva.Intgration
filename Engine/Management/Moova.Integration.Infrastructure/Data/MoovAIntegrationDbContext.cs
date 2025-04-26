f4994be49403d2027c1f2705cfc0a618be5a671c
Hassan Medhat <hassan.medhat@cloud5.tech>
2025-04-26 20:46:06 +0300

    Refactor audit logic and update assembly version
    Refactored audit logic by replacing `AuditInterceptor` in
    `MoovAIntegrationDbContext` with a new `AuditSaveChangesInterceptor`
    class to improve separation of concerns and reusability.
    
    Updated `Program.cs` to conditionally map OpenAPI in development
    environments. Incremented `AssemblyInformationalVersionAttribute`
    to reflect the latest build. Removed redundant audit logic from
    `MoovAIntegrationDbContext`.

--- Engine/Management/Moova.Integration.Infrastructure/Data/MoovAIntegrationDbContext.cs
+++ Engine/Management/Moova.Integration.Infrastructure/Data/MoovAIntegrationDbContext.cs
@@ -1,30 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using Movva.Data.Extensions;
-using Movva.Data.Interceptors;
 
 namespace Moova.Integration.Infrastructure.Data;
 
 public class MoovAIntegrationDbContext(DbContextOptions<MoovAIntegrationDbContext> options) : DbContext(options)
 {
-    private readonly AuditInterceptor _auditInterceptor = new();
-
-    public override int SaveChanges(bool acceptAllChangesOnSuccess)
-    {
-        _auditInterceptor.OnBeforeSaving(ChangeTracker);
-        return base.SaveChanges(acceptAllChangesOnSuccess);
-    }
-
-    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
-    {
-        _auditInterceptor.OnBeforeSaving(ChangeTracker);
-        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
-    }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        builder.ApplyGlobalQueryFilters();
+        builder.ApplyGlobalQueryFilters(); 
         base.OnModelCreating(builder);
     }
 }
