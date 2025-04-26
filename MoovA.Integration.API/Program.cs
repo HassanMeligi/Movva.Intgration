2192f87a02f4c744866d8a6b72ce0a3793f9d81d
Hassan Medhat <hassan.medhat@cloud5.tech>
2025-04-26 21:46:14 +0300

    init

--- MoovA.Integration.API/Program.cs
+++ MoovA.Integration.API/Program.cs
@@ -1,3 +1,5 @@
+using Moova.Integration.Application.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
